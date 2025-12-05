c5ed6e7bd5c0c6f3568c5767bc2e1478eeeae043
Chrishen Silva <chrishensilva@gmail.com>
2025-12-05 19:42:55 +0530

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
-import Home from './Home.jsx';
+import Home from "./Home.jsx";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Helmet } from "react-helmet";
 import { updateSEO } from "./seo";
-
-
+import { Analytics } from "@vercel/analytics/next";
 import React, { useEffect } from "react";
-function App() {
 
+function App() {
   useEffect(() => {
     updateSEO({
       title: "Chrishen Silva | Welcome to official website",
-      description: "Discover and Listen to latest releases and news from Chrishen Silva.",
+      description:
+        "Discover and Listen to latest releases and news from Chrishen Silva.",
       url: "https://chrishensilva.com",
-      image: "/preview.jpg"
+      image: "/preview.jpg",
     });
   }, []);
-
 
   useEffect(() => {
     AOS.init({
-      duration: 800,     // animation duration in ms
-      once: true,        // animate only once
-      easing: 'ease-in-out',
+      duration: 800, // animation duration in ms
+      once: true, // animate only once
+      easing: "ease-in-out",
     });
   }, []);
 
-  
-  return(
-    
+  return (
     <>
-      <Home/>
+      <Home />
     </>
-    
   );
-  
 }
 
-export default App
+export default App;
